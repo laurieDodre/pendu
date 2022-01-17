abf4aab3c706befe7fb9d23ca8c5b7a4a158f4b5
laurieDodre <laurie_dodre_developpeuse_web@outlook.fr>
2022-01-17 14:49:17 +0100

    Partie HTML CSS fini et debut JS

---
+++ script.js
@@ -0,0 +1,26 @@
+//Séléctionner les éléments qui seront utiles (boutons, div qui contiendra le résultat, input de proposition de mots ...)
+// Faire une fonction random qui parcourera le tableau 'secretWord' pour choisir un mot à demasqué au hasard
+//Faire une fonction qui vérifiera le mot proposé dans l'input s'il correspond au mot secret
+//Si les erreurs sont commises alors la partie est terminée, le mot secret s'affiche et le bouton rejouer apparait.
+
+let secretWord = [ //Mon tableau de mots secrets
+    'chocolat',
+    'maison',
+    'believemy',
+    'opportunité',
+    'odeur',
+    'gymnaste',
+    'horizon',
+    'intelligent',
+    'caillot',
+    'bataillon',
+    'bride',
+    'frousse',
+    'porcelaine',
+    'roman',
+    'histoire',
+    'thermometre',
+    'ecole',
+    'fleuriste',
+    'pharmacie'
+]
